f3526c42b2690ce46619d15b15ff715ee13da6a4
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-04-30 16:22:31 +0200

    overlaypic prepare optimization step

--- overlaypic.pl
+++ overlaypic.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl -w
 use strict;
 my $overlay = shift;
-
+my $basepic = "";
 while(<>) {
 	last if m{</svg>};
-	print;
+	$basepic .= $_;
 }
 
 open(my $ovlfd, "<", $overlay) or die "cannot find overlay SVG '$overlay': $!";
@@ -23,11 +23,38 @@ while(<$ovlfd>) {
 }
 close $ovlfd;
 
-my $dx=-7.0;
-my $dy=7.0;
-my $scale=0.099;
-my $angle=0;
-my $mid = 14; # size/2
-print qq'<g transform="rotate($angle, $mid, $mid) translate($dx,$dy) scale($scale)" style="opacity:0.7">', join "\n", @ovl, "</g>";
+sub writeoverlaid(%)
+{
+	my $param=shift;
+	my $mid = 14; # size/2
+	open(my $fd, ">", $param->{filename}) or die $!;
+	print $fd $basepic, qq'<g transform="rotate($param->{angle}, $mid, $mid) translate($param->{dx},$param->{dy}) scale($param->{scale})" style="opacity:0.7">', join "\n", @ovl, "</g>\n</svg>\n";
+	close $fd;
+}
 
-print "</svg>\n";
+my %params=(
+		filename=>"/dev/shm/test.svg",
+		dx=>-7.0,
+		dy=>7.0,
+		scale=>0.099,
+		angle=>0,
+    );
+
+sub optimize(%)
+{
+	my $params = shift;
+	writeoverlaid($params);
+
+#convert -density 450 nice.svg /dev/shm/nice.ppm
+# using PerlMagick to read svg, measure darkened+brightened pixels
+# use gaussian error method
+	use Image::Magick;
+	my $img = new Image::Magick;
+	$img->Set(density=>400, antialias=>0);
+	$img->Read($params->{filename}); # slow part
+	$img->Write("/dev/shm/test.ppm");
+}
+optimize(\%params);
+
+$params{filename}="/dev/stdout";
+writeoverlaid(\%params);
